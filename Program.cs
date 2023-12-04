4c19ded36e87785ace6f357228e4512fd8afcf0f
Dwayne <Dwayne@DWAYNE-PC>
2023-12-04 17:28:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using CombineStrings;
+
+public class Program
+{
+    //This class is for testing the functionality of the CombineStrings program.
+    //The stringsLab.combine method should return the combination of two Strings 
+    //(ex. "String1" + "String2" = "String1String2")
+    static void Main()
+    {
+        Console.WriteLine("The stringLab.combine method should combine two Strings together.");
+
+        //create a new instance of stringLab.
+        stringLab sl = new stringLab();
+
+        //create a couple tests for the stringLab.combine method.
+        Console.WriteLine
+            ("Here's what stringLab.combine returns when provided values 'abc' and 'xyz': ");
+        String p1val1 = "abc";
+        String p1val2 = "xyz";
+        String output1 = sl.Combine(p1val1, p1val2);
+        Console.WriteLine(output1);
+
+        Console.WriteLine
+            ("Here's what stringLab.combine returns when provided value 'hello' and 'world': ");
+        String p2val1 = "hello";
+        String p2val2 = "world";
+        String output2 = sl.Combine(p2val1, p2val2);
+        Console.WriteLine(output2);
+
+    }
+
+}
---
+++ stringLab.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombineStrings
+{
+    public class stringLab
+    {
+     /*
+      * TODO: Return the combination of two Strings.
+      * You combine two Strings using the '+' operator.
+      * For instance, String bigstr = str1 + str2.
+     * This method should return the combination of two Strings.
+     * @param a the first part of the string to be combined.
+     * @param b the second part of the string to be combined.
+     * @return the combination of strings a and b.
+     */
+        
+        //Note: This method could be made static but for the sake of the example
+        //it is left as non-static.
+        public String Combine(String a, String b)
+        {
+            return a + b;
+        }
+
+        //EXTRA: Lambda version commented below, this version is fast 
+        //and compresses the method above into a single line
+
+        /* public String Combine(String a, String b) => a + b; */
+
+    }
+}
